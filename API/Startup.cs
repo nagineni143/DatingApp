16c965a2199d793b5509f25d975dc2c229bedaa1
nagineni1 <hari4chinni@gmail.com>
2021-01-26 18:01:58 +0530

    End of section 3

--- API/Startup.cs
+++ API/Startup.cs
@@ -35,6 +35,7 @@ namespace API
                 options.UseSqlServer(_config.GetConnectionString("DefaultConnection"));
             });
             services.AddControllers();
+            services.AddCors();
 
         }
 
@@ -50,7 +51,7 @@ namespace API
             app.UseHttpsRedirection();
            
             app.UseRouting();
-
+            app.UseCors(policy=>policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
